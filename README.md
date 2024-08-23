0c3a012b9db4558a40f647801780d8e07e1b8623
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-08-23 18:10:08 +1000

    Buttons register on display

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Aug 23 13:17 commit:
 Added number buttons
 Added button input storage (first input numbers are stored)
 
+Aug 23 18:09 commit:
+Made buttons register on display
+Added text box for input so far as well as current input
--- script.js
+++ script.js
@@ -7,6 +7,11 @@ let division = (A,B) => A / B;
 let firstNum = 0;
 let secondNum = 0;
 let operator;
+let currentInput = "0";
+let displayInput = "0";
+const display = document.querySelector(".currInput");
+display.textContent = displayInput;
+
 
 
 function operate(operator, a ,b) {
@@ -32,15 +37,26 @@ function updateDisplay(currentInput, buttonTextContent){
         return currentInput + buttonTextContent;
     }
 }
-let currentInput = "0";
-
 const numberButtons = document.querySelectorAll(".numBtn");
-
 
 numberButtons.forEach((button)=>{
     button.addEventListener("click",function(e){
         currentInput = updateDisplay(currentInput, (e.target).textContent);
+
+        display.textContent = currentInput;
     })
 });
 
+const clearInput = document.querySelector("#clear");
+clearInput.addEventListener("click",function(e){
+    currentInput = "0";
+    display.textContent = currentInput;
+});
+
+const operateButtons = document.querySelectorAll(".operator");
+operateButtons.forEach((button)=>{
+    button.addEventListener("click", function(e){
+        operator = (e.target).textContent;
+    })
+})
 
