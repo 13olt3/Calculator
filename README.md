565e6b71428a527870347c555f59fe6882f14b3c
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-09-02 17:12:31 +1000

    read README

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ Added button input storage (first input numbers are stored)
 Aug 23 18:09 commit:
 Made buttons register on display
 Added text box for input so far as well as current input
+
+Sept 2  17:10 commit:
+<!-- Create a variable to store the first number when an operator is pressed.
+when an operator is pressed, store the first number and clear the entry box
+if an operator is already pressed ( typeof operator = string) then input goes into second number
+evaluate the number by using the fucntions that i wrote earlier based on the operator that was pressed -->
+Got num > operator > num > operator to work
+need to fix num > operator > operator
+need to inplement equals button
+
--- script.js
+++ script.js
@@ -5,11 +5,13 @@ let multiply = (A,B) => A * B;
 let division = (A,B) => A / B;
 
 let firstNum = 0;
-let secondNum = 0;
+let secondNum = "0";
 let operator;
 let currentInput = "0";
 let displayInput = "0";
+let currentTotal;
 const display = document.querySelector(".currInput");
+const topDisplay = document.querySelector(".inputSoFar");
 display.textContent = displayInput;
 
 
@@ -56,7 +58,43 @@ clearInput.addEventListener("click",function(e){
 const operateButtons = document.querySelectorAll(".operator");
 operateButtons.forEach((button)=>{
     button.addEventListener("click", function(e){
-        operator = (e.target).textContent;
+        if (typeof(operator) != 'string'){
+            operator = (e.target).textContent;
+            firstNum = Number(currentInput);
+            currentInput = "0";
+            display.textContent = currentInput;
+            topDisplay.textContent = firstNum + " " + operator;
+        }
+        else{
+            secondNum = Number(currentInput);
+            currentInput = "0";
+            currentTotal = operate(operator,firstNum,secondNum);            
+            operator = (e.target).textContent;
+            display.textContent = currentInput;
+            topDisplay.textContent = firstNum + " " + operator + " " + secondNum + " = " + currentTotal;
+            firstNum = currentTotal;
+        }
     })
 })
+
+const eval = document.querySelector(".eval");
+eval.addEventListener("click", function(e){
+    
+    if(typeof(firstNum) == 'number' && typeof(secondNum) == 'string' && typeof(operator) == 'undefined'){
+        topDisplay.textContent = currentInput;
+        currentInput = 0;
+        display.textContent = currentInput;
+    }
+    // else if(typeof(firstNum) == 'number' && typeof(secondNum) == 'undefined' && typeof(operator) == 'string'){
+    //     topDisplay.textContent = "Enter a second number before evaluating.";        
+    // }
+    // else {
+    //     secondNum = Number(currentInput);
+    //     currentInput = "0";
+    //     currentTotal = operate(operator,firstNum,secondNum);       
+    //     display.textContent = currentInput;
+    //     topDisplay.textContent = firstNum + " " + operator + " " + secondNum + " = " + currentTotal;
+    //     firstNum = currentTotal;
+    // }
+});
 
